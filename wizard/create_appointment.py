bc0c2b46dc11c29fffd37ebbdcada57536604709
manhcuong1122 <cuongdinhmanh1122@gmail.com>
2022-08-03 13:46:04 +0700

    Create record form wizard

--- wizard/create_appointment.py
+++ wizard/create_appointment.py
@@ -5,8 +5,14 @@ class CreateAppointment(models.TransientModel):
     _name = "create.appointment.wizard"
     _description = "Create Appointment Wizard"
 
-    name = fields.Char(string='Name', required=True)
+    date_appointment = fields.Date(string='Date')
     patient_id = fields.Many2one('hospital.patient', string='Patient', require=True)
 
     def action_create_appointment(self):
         print('-------------Button clciked')
+        vals = {
+            'patient_id': self.patient_id.id,
+            'date_appointment': self.date_appointment
+        }
+        print('vals...', vals)
+        self.env['hospital.appointment'].create(vals)
