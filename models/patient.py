df1d53918d853ff0aecb304435df15d8778ccd82
manhcuong1122 <cuongdinhmanh1122@gmail.com>
2022-08-03 16:36:46 +0700

    Add avatar image view

--- models/patient.py
+++ models/patient.py
@@ -21,6 +21,7 @@ class HospitalPatient(models.Model):
 
     appointment_count = fields.Integer('Appointment Count', compute='_compute_appointment_count')
     responsible_id = fields.Many2one('res.partner', string='Responsible')
+    image = fields.Binary(string='Patient Image')
 
     def _compute_appointment_count(self):
         for rec in self:
